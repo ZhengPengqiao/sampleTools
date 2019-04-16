e56dfa4ec87f19af2191b545e316230b74dc9d44
ZhengPengqiao <157510312@qq.com>
2019-04-16 15:27:35 +0800

    add

--- adpcm/adpcm.h
+++ adpcm/adpcm.h
@@ -1,10 +1,9 @@
 #ifndef ADPCM_H
 #define ADPCM_H
 
-struct adpcm_state
-{
-    int valprev;
-    int index;
+struct adpcm_state {
+    short	valprev;	/* Previous output value */
+    char	index;		/* Index into stepsize table */
 };
 
 extern void adpcm_coder(short *indata, signed char *outdata, int len, struct adpcm_state *state, int channels);
