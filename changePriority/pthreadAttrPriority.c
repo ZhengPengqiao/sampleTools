e59e37b32c388a7797377602e4e9f6255575a9ea
ZhengPengqiao <157510312@qq.com>
2019-08-02 02:31:45 +0800

    配置attr,在创建出的线程pthread_create才有用

--- changePriority/pthreadAttrPriority.c
+++ changePriority/pthreadAttrPriority.c
@@ -1,57 +1,117 @@
 /* pthreads_sched_test.c */
-
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
+#include    <stdio.h>  
+#include    <stdlib.h>  
+#include    <unistd.h>  
+#include    <pthread.h>  
+#include    <signal.h>  
+#include    <string.h>  
+#include <sys/syscall.h>
 
 static void display_sched_attr(int policy, struct sched_param *param)
 {
-   printf("    policy=%s, priority=%d\n",
-           (policy == SCHED_FIFO)  ? "SCHED_FIFO" :
-           (policy == SCHED_RR)    ? "SCHED_RR" :
-           (policy == SCHED_OTHER) ? "SCHED_OTHER" :
-           "???",
+    printf("    policy=%s, priority=%d\n",
+           (policy == SCHED_FIFO) ? "SCHED_FIFO" : (policy == SCHED_RR) ? "SCHED_RR" : (policy == SCHED_OTHER) ? "SCHED_OTHER" : "???",
            param->sched_priority);
 }
 
 
-int main(int argc, char *argv[])
-{
-   int s,policy;
-   pthread_attr_t attr;
-   struct sched_param param;
+void * thread_fun(void *arg)  
+{  
+    int policy, ret;  
+    struct sched_param param;  
 
-   /* Initialize thread attributes object according to options */
+    while (1) {  
+        printf("thread_fun pid=%d thread_id=%ld\n", getpid(), syscall(SYS_gettid));
 
+        //获取线程调度参数  
+        ret = pthread_getschedparam(pthread_self(), &policy, &param);  
+        if(ret!=0)  
+        {  
+            printf("pthread_getschedparam %s\n", strerror(ret) );  
+            exit(1);  
+        }
+        display_sched_attr(policy, &param);
 
-    s = pthread_attr_init(&attr);
-    if (s != 0)
-       perror("pthread_attr_init");
+        sleep(1);
+    }
+    pthread_exit(NULL);  
+}  
 
-   policy = SCHED_RR;
-   param.sched_priority = 10;
+int main(int argc,char *argv[])  
+{  
+    int ret;  
+    pthread_t tid;  
+    pthread_attr_t attr;  
+    int policy, inher;  
+    struct sched_param param;  
+  
+    //初始化线程属性  
+    pthread_attr_init(&attr);  
+    //获取继承的调度策略  
+    ret = pthread_attr_getinheritsched(&attr, &inher);  
+    if (ret!=0)  
+    {  
+        printf("pthread_attr_getinheritsched %s\n", strerror(ret));  
+        exit(1);  
+    }  
+  
+    if (inher == PTHREAD_EXPLICIT_SCHED)  
+    {  
+        printf("PTHREAD_EXPLICIT_SCHED\n");  
+    }  
+    else if (inher == PTHREAD_INHERIT_SCHED)  
+    {  
+        printf("PTHREAD_INHERIT_SCHED\n");  
+        inher = PTHREAD_EXPLICIT_SCHED;  
+    }  
 
-   s = pthread_attr_setschedpolicy(&attr, policy);
-   if (s != 0)
-       perror("pthread_attr_setschedpolicy");
-   s = pthread_attr_setschedparam(&attr, &param);
-   if (s != 0)
-       perror("pthread_attr_setschedparam");
+    //设置继承的调度策略  
+    //必需设置inher的属性为 PTHREAD_EXPLICIT_SCHED，否则设置线程的优先级会被忽略  
+    ret = pthread_attr_setinheritsched(&attr, inher);  
+    if (ret!=0)  
+    {  
+        printf("pthread_attr_setinheritsched %s\n", strerror(ret));  
+        exit(1);  
+    }  
 
-   /* If we initialized a thread attributes object, display
-      the scheduling attributes that were set in the object */
+    sleep(1);  
+    policy = SCHED_RR;
+    //设置线程调度策略  
+    ret = pthread_attr_setschedpolicy(&attr, policy);  
+    if (ret!=0)  
+    {  
+        printf(" pthread_attr_setschedpolicy%s\n", strerror(ret));  
+        exit(1);  
+    }  
+    param.sched_priority = 3;  
+    //设置调度参数  
+    ret = pthread_attr_setschedparam(&attr, &param);  
+    if (ret!=0)  
+    {  
+        printf(" pthread_attr_setschedparam %s\n", strerror(ret));  
+        exit(1);  
+    }
+    
+    //创建线程  
+    ret = pthread_create(&tid, &attr, thread_fun, NULL);  
+    if (ret!=0)  
+    {  
+        printf("pthread_create %s\n", strerror(ret));  
+        exit(1);  
+    }  
 
-   s = pthread_attr_getschedparam(&attr, &param);
-   if (s != 0)
-        perror("pthread_attr_getschedparam");
-   s = pthread_attr_getschedpolicy(&attr, &policy);
-   if (s != 0)
-       perror("pthread_attr_getschedpolicy");
-
-   display_sched_attr(policy, &param);
-
-
-   return 0;
+    while (1) {  
+        printf("thread_fun pid=%d thread_id=%ld\n", getpid(), syscall(SYS_gettid));
+        //获取线程调度参数  
+        ret = pthread_getschedparam(pthread_self(), &policy, &param);  
+        if(ret!=0)  
+        {  
+            printf("pthread_getschedparam %s\n", strerror(ret) );  
+            exit(1);  
+        }
+        display_sched_attr(policy, &param);
+        sleep(1);
+    }  
+    pthread_join(tid, NULL);  
+    pthread_exit(NULL);  
 }
